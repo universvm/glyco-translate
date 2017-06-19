c05cbb8ff1b9178583906feb6a3a575309f08e51
universvm <leonardo.castorina98@gmail.com>
2017-06-19 15:30:09 +0200

    started writing the rules in a non-code format

---
+++ main.py
@@ -0,0 +1,14 @@
+#The 14 letter code format is:
+#hexose, hexnac, deoxyhexose, neuac, neugc, pentose, so3h (sulfate), po3h (phosphate), kdn, kdo, hexa #Hexuronate, methyl acetyl, other
+RULES:
+A, antennary = hexnac (glucnac)
+G, galactosylation = hexose (galactose)
+S, sialynation = neuac (or NeuGC)
+F, fucosylation = deoxyhexose (deoxygalactose)
+B, bisection = hexnac (glucnac)
+Core F = 2 hexnac (glucnac) + 3 hexose (mannose) + 1 deoxyhexose (deoxygalactose)
+Man = hexose
+#keep the n-n2 
+
+
+# if n > 9 number with ,
