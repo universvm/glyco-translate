5a25cb70541f98f001bdc136ae0472116eb6a500
universvm <leonardo.castorina98@gmail.com>
2017-06-21 09:50:01 +0200

    began coding the "real" translation

--- main.py
+++ main.py
@@ -10,9 +10,9 @@
 # Core F = 2 hexnac (glucnac) + 3 hexose (mannose) + 1 deoxyhexose (deoxygalactose)
 # Man = hexoses
 # Hy = 3 hexnac + 5 hexose
-# M = 2 hexnac + 5 hexose
+# M = 2 hexnac + X hexose
 # C = 4 hexnac + 3 hexose
-
+import re
 
 #keep the n-n2
 
@@ -28,7 +28,6 @@ def translator(oxglycan):
             print(g)
         else:
             g1 = g.rsplit(' ', 1)
-            print(g1)
             if "Man" in g1[0]:
                 man = g1[0].split("Man") #splitting by Man to get a list
                 # print(man)
@@ -40,19 +39,22 @@ def translator(oxglycan):
                 else: #else man[1] is a single integer
                     man[1] += "0" *(14 -len(man[1]))
                     print(man[1])
-            # elif "M" in g:
-                #
+            elif "M" in g1[0]:
+                ma = g1[0].split("M") #splitting by M to get a list
+                if "-" in ma[1]: # if there is a range
+                    num = ma[1].split("-")
+                    num[0] += "2"
+                    num[0] += "0" *(14 -len(num[0]))
+                    num[1] += "0" *(14 -len(num[1]))
+                    print(num[0]);print(num[1])
+                else: #else there is no range
+                    ma[1] += "0" *(14 -len(ma[1]))
+                    print(ma[1])
             elif "Hy" in g:
                 print("53000000000000")
             else:
-                print(g1[0])
-        # if "Man" or "Hy" not in g:
-        #     print(g)
-        # else:
-        #     print("ELSE")
-
-
-
+                gly = re.split(r'([A-Z][a-z]*\d *)', g1[0])
+                print(gly)
 for line in table:
     line = line.split(",")
     glycans = ''.join([x for x in line[7] if ord(x) < 128]) #Thanks to: http://snipplr.com/view/57881/
